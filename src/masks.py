31f0a3cc8a9adc0595fd94a48dc1135d96b48a44
Said Magomedov <saidmagomedov037@gmail.com>
2024-12-20 19:05:53 +0500

    change type annotation

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,10 @@
-def get_mask_card_number(card_number: str):
+def get_mask_card_number(card_number: str) -> str:
     '''принимает на вход номер карты и возвращает ее маску'''
     mask_card_number = card_number[0:7]+'******'+card_number[-4:]
-    return mask_card_number: str
+    return mask_card_number
 
 
-def get_mask_account(account: str):
+def get_mask_account(account: str) -> str:
     '''принимает на вход номер счета и возвращает его маску'''
     mask_account = '**'+account[-4:]
-    return mask_account: str
+    return mask_account
--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,14 @@
-def mask_account_card(account_card: str):
+def mask_account_card(account_card: str) -> str:
     '''функция возвращает замаскированный номер'''
-    if account_card[0;4] == 'Счет':
+    if account_card[0:4] == 'Счет':
         mask_account = '**'+account_card[-4:]
-        return mask_account: str
+        return mask_account
 
     else:
         mask_card_number = account_card[0:7] + '******' + account_card[-4:]
         return mask_card_number: str
 
-def get_date(date_data: str):
+def get_date(date_data: str) -> str:
     '''функция форматирует данные о дате'''
     new_date = date_data[8;10]+'.'+date_data[5;7]+'.'+date_data[0:4]
-    return new_date: str
+    return new_date
