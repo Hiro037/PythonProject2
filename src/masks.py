6130d2b3309b099b576fbe222d1b5daba065d59d
Said Magomedov <saidmagomedov037@gmail.com>
2025-03-05 15:04:47 +0500

    main.py added main
    regex_utils.py added

--- src/masks.py
+++ src/masks.py
@@ -24,6 +24,7 @@ def get_mask_card_number(card_number: Union[str, int]) -> str:
             try:
                 logger.info('Функция вернула маску номера карты')
                 mask_card_number = str(card_number)[0:6]+'******'+str(card_number)[-4:]
+                mask_card_number = str(card_number)[0:4] + ' ' + str(card_number)[4:6] + '** **** ' + str(card_number)[-4:]
                 return mask_card_number
             except Exception as e:
                 logger.warning(f'Произошла ошибка: {e}')
